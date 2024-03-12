00d0b79c6d737e681d15d18518449b99dc41b59b
Alexander Skachko <alexanderskachko@Alexanders-MacBook-Pro.local>
2024-03-12 16:54:48 -0400

    trying out package

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
 use termion::{color, style};
 
+use std::io;
+
 fn main() {
-    println!("Hello, there!");
+    println!("{}red", color::Fg(color::Red));
+    println!("{}blue", color::Fg(color::Blue));
+    println!("{}Yellow", color::Fg(color::Yellow));
 }
